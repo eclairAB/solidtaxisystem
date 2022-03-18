e819d6223f8095d853caef757535767c47920410
ben <alanbenedictgolpeo@gmail.com>
2022-03-19 04:43:37 +0800

    updateee

---
+++ js/billing.js
@@ -0,0 +1,32 @@
+var modal = document.getElementById("myModal");
+
+// var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal_ii) {
+    modal_ii.style.display = "none";
+  }
+}
+
+function cashierBillingOnZeroes() {
+	const element = document.querySelector("table.cart tbody tr:last-child")
+	if(element) {
+		const price = element.children[1].innerHTML
+		if (parseFloat(price) <= 0) {
+			console.log(123)
+			modal_ii.style.display = "block";
+		}
+	}
+}
+
+cashierBillingOnZeroes()
